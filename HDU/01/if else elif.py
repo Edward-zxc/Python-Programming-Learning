1262d7d4e6a74e15b23d9fdf8593b1b50a0a51a1
Edward xc <3313338649@qq.com>
2024-03-31 14:15:14 +0800

    jjjjj

---
+++ HDU/01/if else elif.py
@@ -0,0 +1,20 @@
+def falling(n, k):
+    result = 1
+    for i in range(0,k):
+        result *= (n - i)
+    return result
+
+# Example usage:
+print(falling(6, 3))  # Output: 6 * 5 * 4 = 120
+
+def falling(n, k):
+    result = 1
+    i = 0
+    while i < k:
+        result *= (n - i)
+        i += 1
+    return result
+
+# Example usage:
+print(falling(6, 3))  # Output: 6 * 5 * 4 = 120
+
---
+++ Lab01/if -for -while/login.py
@@ -0,0 +1,7 @@
+account = input("Enter your account")
+password = input("Enter your password")
+if account == "admin" and password == "<PASSWORD>":
+    print("Access granted")
+    print("You are now logged in")
+elif account == "admin" and password == "<PASSWORD>":
+    print("Access")
