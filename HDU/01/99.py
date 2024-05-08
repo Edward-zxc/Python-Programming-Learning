ce9caccdd234443908264ba694e31a20e6e6bafa
Edward xc <3313338649@qq.com>
2024-05-08 18:15:17 +0800

    Update for HDU Lab01

--- HDU/01/99.py
+++ HDU/01/99.py
@@ -1,4 +1,25 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
-def func5(a, b, *c):
-   print(a,b)
-func5(1,2,3,4,5,6)
+
+def is_valid(s):
+    length = len(s)
+    if 2 <= length <= 6:
+        if s[0:2].isalpha():
+            for i in range(1, length - 1):
+                if s[i].isdigit():
+                    if s[i - 1].isalpha() and s[i + 1].isalpha():
+                        return False
+            return True
+        for i in range(length):
+            if not s[i].isalpha() or not s[i].isdigit():
+                return False
+    else:
+        return False
+
+
+main()
