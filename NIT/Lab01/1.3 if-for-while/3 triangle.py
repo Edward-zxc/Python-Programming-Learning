f54e9dc459a0d497aa017f160afa146384ba815e
Edward xc <3313338649@qq.com>
2024-05-21 19:07:31 +0800

    Update for nit lab2

--- NIT/Lab01/1.3 if-for-while/3 triangle.py
+++ NIT/Lab01/1.3 if-for-while/3 triangle.py
@@ -1,11 +1,4 @@
-height = int(input("Height: "))
-
-for i in range(1,height-1):
-    for j in range(1,height-i-1):
-        print(" ",end="")
-    for k in range(1,i+1):
-        print("#",end="")
-    print(" ")
-    for l in range(1,i+1):
-        print("#",end="")
-    print()
+n = int(input("Height: "))
+for i in range(n):
+    print(' ' * (n - i - 1), end='')
+    print('*' * (2*i + 1))
